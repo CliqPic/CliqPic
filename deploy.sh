52dc35df6dda747476ddd7e82cde0cecd55b0b35
Chris Hall <chall8908@gmail.com>
2017-04-12 18:46:29 -0500

    Add new deploy script
    Remove old deploy zip; using the EB CLI is so much easier

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+help() {
+    echo -e "This command automates the deploy process for this application"
+    echo -e "Usage: deploy.sh [current_version] [new_version]"
+}
+
+if [ -z $1 ]
+then
+    echo -e "No arguments supplied\n"
+    help
+    exit 1
+fi
+
+if [ -z $2 ]
+then
+    echo -e "New version not specified\n"
+    help
+    exit 1
+fi
+
+# Update the version numbers located in the Dockerfile, compose config, and Beanstalk config
+sed -i s/$1/$2/ {Dockerfile,docker-compose.yml,Dockerrun.aws.json} 2>/dev/null
+
+# Commit those changes
+git commit -am "Bump version to $2"
+
+# and tag them
+git tag -a v$2 -m "cliq-pic version $2"
+
+# log into the container registry
+sudo $(aws ecr get-login)
+
+sudo docker-compose build server
+
+# Tag the new image correctly so we can push it to the registry
+sudo docker tag amdirent/cliq-pic:$2 402239435993.dkr.ecr.us-east-1.amazonaws.com/clients/cliq-pic:$2
+
+# Push it to the registry
+sudo docker push 402239435993.dkr.ecr.us-east-1.amazonaws.com/clients/cliq-pic:$2
+
+# Finally, deploy to beanstalk
+eb deploy -l "cliq-pic-$2"
