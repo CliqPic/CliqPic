4afe1eb8a1a18a13093b023e36145c9652be8772
Jordan Prince <jordanmprince@gmail.com>
2017-04-13 15:17:14 -0500

    Add dotenv

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load if Rails.env.development? or Rails.env.test?
 
 module CliqPiq
   class Application < Rails::Application
