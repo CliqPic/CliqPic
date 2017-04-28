e561a23f5654c4cc4b432a73e57c7d9623f9e8a9
Jordan Prince <jordanmprince@gmail.com>
2017-04-28 15:38:47 -0500

    Trying agin

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-if Rails.env.production?
-  Rails.application.config.assets.manifest = "/data/amdirent/cliq-pic/app/assets/config/manifest.js"
-end
+#if Rails.env.production?
+#  Rails.application.config.assets.manifest = "/data/amdirent/cliq-pic/app/assets/config/manifest.js"
+#end
