fdf82b6f2b6356631659e10838de5c1879c5bd99
Chris Hall <chall8908@gmail.com>
2017-05-12 12:41:19 -0500

    Redirect unauthenticated users to cliq-pic website in prod

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,21 @@
 class StaticController < ApplicationController
-  def index
-    puts Rails.application.config.assets.manifest
+  before_action :redirect_signed_in, only: [ :index ]
 
+  if Rails.env.production?
+    before_action :redirect_prod_login, only: [ :index ]
+  end
+
+  def index
+  end
+
+  def redirect_signed_in
     redirect_to dashboard_path if user_signed_in?
+  end
+
+  # This only runs when Rails is running in production
+  def redirect_prod_login
+    # Ensure that no flash messages are carried over to the next login
+    flash.clear
+    redirect_to 'http://www.cliq-pic.com'
   end
 end
